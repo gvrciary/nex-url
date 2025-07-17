f3bba3bea08c229a8d8fbce97c53e127fd31d439
AlexisGxrcia <179990131+alexisgxrcia@users.noreply.github.com>
2025-07-17 00:38:22 -0700

    chore: rename temporarily v3

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Settings from "@/components/settings/Settings";
+import Settings from "@/components/setting/settingsT";
 import { LinksProvider } from "@/components/providers/links-provider";
 
 export default function SettingsPage() {
