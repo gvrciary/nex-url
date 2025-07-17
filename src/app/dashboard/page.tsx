70c0275b114aeadf2e1119cbc4031f6a292e7765
AlexisGxrcia <179990131+alexisgxrcia@users.noreply.github.com>
2025-07-17 12:16:21 -0700

    chore: add metadata titles to dashboard and settings pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
-"use client";
-
 import AddLink from "@/components/dashboard/add-link";
 import LinkHistory from "@/components/dashboard/link-history";
 import { LinksProvider } from "@/components/providers/links-provider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DashboardL",
+};
 
 export default function DashboardPage() {
   return (
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-
 import Settings from "@/components/setting/settings";
 import { LinksProvider } from "@/components/providers/links-provider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SettingsL",
+};
 
 export default function SettingsPage() {
   return (
